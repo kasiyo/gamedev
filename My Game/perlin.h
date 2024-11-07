3d8027aa18dc1dfa792f360456dbca065c4f65df
Burrier, Donald <donaldburrier@my.unt.edu>
2024-11-07 05:13:19 +0000

    Add new file

---
+++ My Game/perlin.h
@@ -0,0 +1,20 @@
+#ifndef PERLIN_H
+#define PERLIN_H
+
+class Perlin {
+public:
+    Perlin();
+    ~Perlin();
+
+    // Generates a Perlin noise value between -1 and 1 at the given 3D position.
+    float noise(float x, float y, float z);
+
+private:
+    int* p; // Permutation table
+    float* Gx, * Gy, * Gz; // Gradient vectors
+
+    void initialize();  // Initializes permutation and gradient arrays
+};
+
+
+#endif
